9e56e5c0b5010ab53310a6589b6d898ab0885ab4
abdulmajeedsualihu <abdulmajeedsualihu2000@gmail.com>
2024-11-08 23:06:53 +0000

    Initial Upload

---
+++ HelloArbitrum.sol
@@ -0,0 +1,17 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.26;
+
+
+contract HelloArbitrum {
+    string public message;
+
+    // Constructor to initialize the contract with a message
+    constructor(string memory _message) {
+        message = _message;
+    }
+
+    // Function to update the message
+    function updateMessage(string memory _newMessage) public {
+        message = _newMessage;
+    }
+}
